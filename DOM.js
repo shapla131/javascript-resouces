c0e1b7a3aa7f88c845b6542024ffe741a865a4c6
Shapla <shapla@Shaplas-MacBook-Air.local>
2023-05-08 04:29:12 +0600

    30-35

---
+++ DOM.js
@@ -0,0 +1,5 @@
+// document.getElementById("head1").innerHTML="shapla";
+
+// document.getElementById("para").innerHTML="hello";
+//document.getElementsByClassName("para")[0].innerHTML="bangladesh";
+document.getElementsByClassName("para")[1].innerHTML="hello";
---
+++ query selecrtor.js
@@ -0,0 +1,9 @@
+document.querySelector(".para").innerHTML="shapla";
+document.querySelector("#para").innerHTML="hello";
+
+document.querySelector("a").innerHTML="text";
+document.querySelector("li a").innerHTML="hi";
+document.querySelector(".new-div a").innerHTML="hello";
+
+document.querySelectorAll("p")[1].innerHTML="good";
+document.querySelectorAll("p")[2].innerHTML="bye";
